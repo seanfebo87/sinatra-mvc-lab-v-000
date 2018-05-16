d802cdef6b8b6d74cc2e13e26cca17aa378bc32c
Sean Febo <seanfebo@techie.com>
2018-05-16 19:15:45 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -10,7 +10,7 @@ class PigLatinizer
     elsif consonants.include?(word[0]) && consonants.include?(word[1])
     word[2..-1] + word[0..1] + 'way'
     elsif consonants.include?(word[0])
-     word[1..-1] + word[0] + 'way'
+     word[1..-1] + word[0] + 'ay'
     else
     word
     end
