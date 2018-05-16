be99656ebf4009dfc8ec85e8cc5fed346ae9acfb
Sean Febo <seanfebo@techie.com>
2018-05-16 19:43:15 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,7 +6,7 @@ class PigLatinizer
    vowels = %w(a e i o u)
    consonants = alpha - vowels
     if consonants.include?(word[0]) && consonants.include?(word[1]) && consonants.include?(word[2])
-      word[3..-1] + word[0..1] + 'ay'
+      word[3..-1] + word[0..2] + 'ay'
     elsif consonants.include?(word[0]) && consonants.include?(word[1])
     word[2..-1] + word[0..1] + 'ay'
      elsif consonants.include?(word[0])
