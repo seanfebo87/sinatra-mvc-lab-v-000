1b1c4ff3b3bbda8c570801b69b03fc905af1acb8
Sean Febo <seanfebo@techie.com>
2018-05-16 20:34:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,11 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-  get '/' do
+  get "/" do
     erb :user_input
   end
   
-  post '/piglatinize' do
+  post "/piglatinize" do
     r = Piglatizizer.new
     @result = r.piglatinize(params[:user_phrase])
     erb :results
