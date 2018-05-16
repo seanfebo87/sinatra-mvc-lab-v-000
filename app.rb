e00badaebf1edfb00f6258ae5aa520535aa7d008
Sean Febo <seanfebo@techie.com>
2018-05-16 20:36:24 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
   
   post "/piglatinize" do
     r = Piglatizizer.new
-    @result = r.piglatinize(params[:user_phrase])
+    @result = r.pigLatinize(params[:user_phrase])
     erb :results
   end
 end
